5f0fd11b85e137eded63779f490f5347dfcb474a
Ricardo Longa <ricardo.longa@gmail.com>
2021-01-18 11:21:02 -0300

    Update deploy script and event date.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 hugo -t=gopherconbr
 hugo -t=gopherconbr --config=config_en.toml
+git submodule add -b master git@github.com:gopherconbr/gopherconbr.github.io.git public
 cd public
 git add .
 git commit -m "updating site"
